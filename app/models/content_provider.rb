7f147509a6d5b52378ea7fff4b11c0f5f3e00aad
Sergei Mellow <sergei.mellow@gmail.com>
2016-03-31 22:56:57 -0400

    bare bones are up and running. 44 free clips/espodes, just links and corect relationships, ie cn > show > ep, 1 cn 11 shows, 44 eps

--- app/models/content_provider.rb
+++ app/models/content_provider.rb
@@ -38,12 +38,32 @@ class ContentProvider < ActiveRecord::Base
   		end
   	end
 
-  	# will interate over each show and for each show grab all links that
-  	# are to a free episode that does not require a login to watch on the site
+  	# will iterate over each show and for each show or clip and grab all links that
+  	# are to a free content that does not require a login to watch on the site
   	def get_all_free_episode_links_for_each_show_on_vland
   		Show.all.each do |show|
+  			puts "getting epsides for #{show.url}"
   			# sometimes its nice to take a 2-3 sec nap
   			sleep(rand(2..3))
+  			resp = get_resp(show.url)
+  			# this is the point where I got tired of heavy lifting
+  			html_result = Nokogiri::HTML.parse(resp.body)
+  			html_result.css(".entry-card").each do |clip|
+  				# skip episdes / clips that are not free to watch, may also one day limit it to just episodes.
+  				next if clip.css(".unlockable").first.to_s.include? "icon-locked"
+  				# grabs sub link to epsides or clip
+  				episode_or_clip_url = clip.css(".cta").attr("href").to_s
+  				#getting rid of a "/" at the start
+  				episode_or_clip_url[0] = ""
+
+  				# lets create some links pertaining to shows and clips from shows
+  				ep = Episode.find_or_initialize_by(url: self.url + episode_or_clip_url)
+  				if ep.new_record?
+  					ep.show_id=show.id
+  					#can do fancy stuff here like grab all show episode / clip fields 
+  					ep.save!
+  				end
+  			end
   		end
   	end
 
